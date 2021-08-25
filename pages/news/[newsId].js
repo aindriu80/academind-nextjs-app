300d21a1e6e09d6695ced0e8fd7bc85986d1cf98
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2021-08-25 15:13:30 +0100

    Extracting Dynamic Parameter Values

--- pages/news/[newsId].js
+++ pages/news/[newsId].js
@@ -1,6 +1,16 @@
+import { useRouter } from 'next/router'
+
 // our-domain.com/news/something-important
 
 function DetailPage() {
+  const router = useRouter()
+
+  console.log(router.query.newsId)
+  const newsId = router.query.newsId
+
+  // send a request to the backend API
+  // to fetch the news item with newsId
+
   return <h1>Hello from the News Details Page</h1>
 }
 export default DetailPage
