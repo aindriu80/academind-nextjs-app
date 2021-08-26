0a8d74b5422c7de6584c360a1dad0e6351c694db
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2021-08-26 15:50:14 +0100

    Outputting a list of meetups

---
+++ pages/index.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import MeetupList from '../components/meetups/MeetupList'
+
+const DUMMY_MEETUPS = [
+  {
+    id: 'm1',
+    title: 'New York',
+    image: './img/photo-1500916434205-0c77489c6cf7.webp',
+    address: 'Lower Manhattan',
+    description: 'This is the initial meetup!!',
+  },
+  {
+    id: 'm2',
+    title: 'Chicago',
+    image: './img/photo-1629902040617-f58a5a15797c.webp',
+    address: 'Chicago, Chicago',
+    description: 'This is the second meetup!!',
+  },
+]
+
+const HomePage = () => {
+  return <MeetupList meetups={DUMMY_MEETUPS} />
+}
+
+export default HomePage
