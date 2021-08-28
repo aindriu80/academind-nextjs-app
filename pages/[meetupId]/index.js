4a6832c85f5c37dda605d733fa98f6372bc9684e
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2021-08-28 11:37:53 +0100

    Adding Custom Components & CSS Modules

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,1 +1,14 @@
-// our-domain.com/new-meetup
+import MeetupDetail from '../../components/meetups/MeetupDetail'
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="./img/photo-1500916434205-0c77489c6cf7.webp"
+      title="First Meetup"
+      address="Some Street by Some City"
+      description="This is the first meetup"
+    />
+  )
+}
+
+export default MeetupDetails
