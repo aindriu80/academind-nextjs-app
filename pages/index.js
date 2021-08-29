51491b2c13ca47af04e7058a0980f3b21c135a16
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2021-08-29 16:33:15 +0100

    Data fetching for static pages

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { React, useEffect, useState } from 'react'
+import { React } from 'react'
 import MeetupList from '../components/meetups/MeetupList'
 
 const DUMMY_MEETUPS = [
@@ -18,14 +18,17 @@ const DUMMY_MEETUPS = [
   },
 ]
 
-const HomePage = () => {
-  const [loadedMeetups, setLoadedMeetups] = useState([])
-  useEffect(() => {
-    // send a http request and fetch data
-    setLoadedMeetups(DUMMY_MEETUPS)
-  }, [])
+const HomePage = (props) => {
+  return <MeetupList meetups={props.meetups} />
+}
 
-  return <MeetupList meetups={loadedMeetups} />
+export function getStaticProps() {
+  // fetch data from an API
+  return {
+    props: {
+      meetups: DUMMY_MEETUPS,
+    },
+  }
 }
 
 export default HomePage
