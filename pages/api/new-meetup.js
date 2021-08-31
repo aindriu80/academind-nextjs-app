7fc34148eb22ce98dff06d470db37808895d2cc5
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2021-08-31 16:37:53 +0100

    Working with MongoDB - initial

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,11 +1,24 @@
+import { MongoClient } from 'mongodb'
 // /api/new-meetup
 // POST /api/new-meetup
 
-function handler(res, req) {
+async function handler(res, req) {
   if (req.method === 'POST') {
     const data = req.data
 
-    const { title, image, address, description } = data
+    const client = await MongoClient.connect(
+      'mongosh "mongodb+srv://cluster0.2ckgy.mongodb.net/meetups" --username user'
+    )
+    const db = client.db()
+    const meetupsCollection = db.collection('meetups')
+
+    const result = await meetupsCollection.insertOne(data)
+
+    console.log(result)
+
+    client.close()
+
+    res.status(201).json({ message: 'Meetup inserted ok' })
   }
 }
 export default handler
